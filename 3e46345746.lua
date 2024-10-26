afa1582ba300c06d9652a238a50a2066074bb5de
Pythen <93877964+PythenStatus@users.noreply.github.com>
2024-10-26 15:29:40 +0200

    3e46345746.lua

--- 3e46345746.lua
+++ 3e46345746.lua
@@ -12,13 +12,6 @@ while  wait(1) do
 coroutine.resume(coroutine.create(function()
 for _,v in pairs(game[players]:GetPlayers()) do
 if v.Name ~= plr.Name and v.Character then
-v.Character.RightUpperLeg.CanCollide = false
-v.Character.RightUpperLeg.Transparency = 10
-v.Character.RightUpperLeg.Size = Vector3.new(13,13,13)
- 
-v.Character.LeftUpperLeg.CanCollide = false
-v.Character.LeftUpperLeg.Transparency = 10
-v.Character.LeftUpperLeg.Size = Vector3.new(13,13,13)
  
 v.Character.HeadHB.CanCollide = false
 v.Character.HeadHB.Transparency = 10
@@ -26,7 +19,7 @@ v.Character.HeadHB.Size = Vector3.new(13,13,13)
  
 v.Character.HumanoidRootPart.CanCollide = false
 v.Character.HumanoidRootPart.Transparency = 10
-v.Character.HumanoidRootPart.Size = Vector3.new(13,13,13)
+v.Character.HumanoidRootPart.Size = v.Character.HumanoidRootPart.Size
  
 end
 end
